5e36e7f68b6e92bdcbb72bffca6027704d3893bc
Lukas Ruebbelke <simpul@gmail.com>
2015-08-08 21:43:49 -0700

    Clean up

--- realtime/js/realtime.js
+++ realtime/js/realtime.js
@@ -3,11 +3,11 @@ var app = angular.module('realtimeApp', ['firebase']);
 app.constant('RESTFUL_URI', 'https://realtime-event-bus.firebaseio.com/');
 app.constant('FIREBASE_URI', 'https://realtime-event-bus.firebaseio.com/');
 
-app.controller('RealtimeCtrl', function ($scope, RESTService, RealtimeService, CurrentOrderService) {
+app.controller('RealtimeCtrl', function ($scope, RealtimeService, CurrentOrderService) {
   var realtime = this;
 
   realtime.orders = RealtimeService.all();
-  realtime.currentOrder = CurrentOrderService.getCurrentOrder();
+  realtime.currentOrder = CurrentOrderService.currentOrder;
 
   $scope.$on('currentOrderUpdated', function () {
     realtime.currentOrder = CurrentOrderService.getCurrentOrder();
@@ -15,7 +15,7 @@ app.controller('RealtimeCtrl', function ($scope, RESTService, RealtimeService, C
 });
 
 app.directive('order',
-  function ($rootScope, $firebaseObject, RESTService, FIREBASE_URI, CurrentOrderService) {
+  function ($firebaseObject, FIREBASE_URI, RESTService, CurrentOrderService) {
     var controller = function () {
       var orderCtrl = this,
         ref = new Firebase(FIREBASE_URI + 'realtime-orders/' + orderCtrl.orderId);
@@ -65,7 +65,7 @@ app.factory('CurrentOrderService', function ($rootScope) {
   };
 });
 
-app.factory('RealtimeService', function ($firebaseArray, $firebaseObject, FIREBASE_URI) {
+app.factory('RealtimeService', function ($firebaseArray, FIREBASE_URI) {
   var ref = new Firebase(FIREBASE_URI + 'realtime-orders');
   var orders = $firebaseArray(ref);
 
@@ -73,29 +73,8 @@ app.factory('RealtimeService', function ($firebaseArray, $firebaseObject, FIREBA
     return orders
   };
 
-  var create = function (id) {
-    var ref = new Firebase(FIREBASE_URI + 'realtime-orders/' + id);
-    var order = $firebaseObject(ref);
-    order.updated_at = new Date().toString();
-    order.$save();
-  };
-
-  var save = function (id) {
-    var order = orders.$getRecord(id);
-    order.updated_at = new Date().toString();
-    orders.$save(order);
-  };
-
-  var destroy = function (id) {
-    var order = orders.$getRecord(id);
-    orders.$remove(order);
-  };
-
   return {
-    all: all,
-    create: create,
-    save: save,
-    destroy: destroy
+    all: all
   };
 });
 
